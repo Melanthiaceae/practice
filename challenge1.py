6904deba7329f3a45ff2e12881608b00c7048825
Melanthiaceae <31169438+Melanthiaceae@users.noreply.github.com>
2017-08-19 23:11:42 +0100

    Create challenge1.py

---
+++ challenge1.py
@@ -0,0 +1,41 @@
+alphabet = {
+  1: "a",
+  2: "b",
+  3: "c",
+  4: "d",
+  5: "e",
+  6: "f",
+  7: "g",
+  8: "h",
+  9: "i",
+  10: "j",
+  11: "k",
+  12: "l",
+  13: "m",
+  14: "n",
+  15: "o",
+  16: "p",
+  17: "q",
+  18: "r",
+  19: "s",,
+  20: "t",
+  21: "u",
+  22: "v",
+  23: "w",
+  24: "x",
+  25: "y",
+  26: "z"
+}
+
+enc_message = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+message = []
+
+for c in enc_message:
+  if c = " " or c = "." or c = "'" or c = "(" or c = ")":
+    message.append(c)
+  else:
+    enc_key = alphabet.get(c,"?")
+    real_key = alphabet[enc_key + 2]
+    message.append(alphabet[real_key])
+    
+print "".join(message)
